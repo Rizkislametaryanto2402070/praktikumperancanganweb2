7f651b7bf986739a076e10bb3bb351324a88838e
rizki slamet aryanto <rizkislametaryanto@gmail.com>
2025-12-10 11:09:19 +0700

    Add database connection script in conn.php

---
+++ praktikum9(pagination)/codingan/conn.php
@@ -0,0 +1,12 @@
+<?php
+// conn.php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "pagination";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
